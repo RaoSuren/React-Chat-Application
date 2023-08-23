620b183764694c335785f221034eff9849d59db4
CodeSandbox <support@codesandbox.io>
2023-08-23 16:24:52 +0000

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,34 +1,36 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
+  cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+  console.log(`User Connected: ${socket.id}`);
 
-    socket.on("join_room", (data) =>{
-        socket.join(data);
-        console.log(`Joined Room with Room: ${data}`);
-    });
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`Joined Room with Room: ${data}`);
+  });
 
-    socket.on("send_message", (data)=> {
-        socket.to(data.room).emit("receive_message", data);
-    })
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-    socket.on("disconnect", ()=>{
-        console.log("User Disconnected", socket.id);
-    });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
 });
 
-server.listen()
+server.listen(3001, () => {
+  console.log("Server Running on Port 3001");
+});
